e5d5829ae7609d2b20e730a988de5fe5508ba8b1
Alex Willis <willisalex@hotmail.co.uk>
2025-08-29 16:42:45 +0000

    Set the screen to be noprojectselected when a new project is added and styled the projects in the sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,9 +12,13 @@ export default function Sidebar({ onStartAddProject, projects }) {
       </div>
 
       {projects && (
-        <ul>
+        <ul className="mt-8">
           {projects.map((project) => (
-            <li key={project.id}>{project.title}</li>
+            <li key={project.id}>
+              <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
+                {project.title}
+              </button>
+            </li>
           ))}
         </ul>
       )}
